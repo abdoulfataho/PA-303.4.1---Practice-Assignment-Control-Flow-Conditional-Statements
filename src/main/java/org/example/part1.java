5d8159a5b1115116a50cbdb55f986ec00260f9ab
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-03 09:01:14 -0400

    project created and part one solved

---
+++ src/main/java/org/example/part1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class part1 {
+    public static void main(String[] args) {
+        int x;
+        x = 15;
+        if (x < 10) {
+            System.out.println("Less than ten");
+        }
+
+
+    }
+}
