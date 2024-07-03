5c56668858b03f0b6f80c66ad48cf6c61d6440ae
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-03 09:10:27 -0400

    part two if else statement

---
+++ src/main/java/org/example/part2.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class part2 {
+    public static void main(String[] args) {
+        int x;
+        x = 15;
+        if (x < 10) {
+            System.out.println("Less than 10");
+        } else {
+            System.out.println("Greater than ten");
+        }
+    }
+}
