f71bc31c888a9e93f9fc2336e179829579943fbc
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-03 11:28:31 -0400

    part3

---
+++ src/main/java/org/example/part3.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class part3 {
+    public static void main(String[] args) {
+        int x;
+        x = 50;
+        if (x < 10) {
+            System.out.println("Less than 10");
+        } else if (x>10 & x <20){
+            System.out.println("Between 10 and 20");
+        }
+        else if  (x >20) {
+            System.out.println("Greater than 20");
+        }
+
+    }
+}
