6045b8bbeb30e601b9b3c8c088500a80050c77ab
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-03 11:39:06 -0400

    part 4

---
+++ src/main/java/org/example/part4.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class part4 {
+    public static void main(String[] args) {
+        int x;
+        x=5;
+        if (x < 10) {
+            System.out.println("Out of range");
+        } else if (x>=10 & x <=20){
+            System.out.println("In range");
+        }
+        else if  (x >20) {
+            System.out.println("Out of range");
+        }
+
+    }
+}
